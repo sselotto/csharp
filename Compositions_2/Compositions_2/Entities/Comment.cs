5b54e6de7354d9a888d73f1c91aadd7caac065dd
Samir Selotto <samir.selotto@gmail.com>
2021-05-21 09:13:11 -0300

    Second Commit

---
+++ Compositions_2/Compositions_2/Entities/Comment.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Compositions_2.Entities
+{
+    class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment() //Construtor sem argumento
+        {
+
+        }
+
+        public Comment(string text) //Construtor
+        {
+            Text = text;
+        }
+    }
+}
