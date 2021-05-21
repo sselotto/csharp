0456cb5df14c4b5cf23e192cf83ed74767f48bb4
Samir Selotto <samir.selotto@gmail.com>
2021-05-21 16:52:11 -0300

    Sixth Commit

---
+++ Sobreposicao_Override/Sobreposicao_Override/Entities/SavingsAccount.cs
@@ -0,0 +1,29 @@
+﻿namespace Sobreposicao_Override.Entities
+{
+    class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount()
+        {
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate)
+            : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+
+        public override void Withdraw(double amount)
+        {
+            base.Withdraw(amount); // Vou chamar a operação de saque na superclasse (base)
+            Balance = Balance - 2.0;
+        }
+
+    }
+}
