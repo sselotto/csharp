96518ea052370f80ce8836ca8597c7366c8ac7cc
Samir Selotto <samir.selotto@gmail.com>
2021-05-23 19:39:23 -0300

    Eleventh Commit

---
+++ Tratamento_Excecoes/Tratamento_Excecoes/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace Tratamento_Excecoes.Entities.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+        //Construtor recebendo uma string message
+        //Repassa essa mensagem para o construtor da classe applicationException
+
+        public DomainException(string message) : base(message)
+        {
+
+        }
+    }
+}
+    
+
