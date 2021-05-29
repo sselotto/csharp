1e6eb03e2675247bd21d9945d383ee4a345fa337
Samir Selotto <samir.selotto@dellead.com>
2021-05-29 16:09:48 -0300

    seventeenth Commit

---
+++ LINQ/LINQ/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Specify the data source
+
+            int[] numbers = new int[] { 2, 3, 4, 5 };
+
+            // Define the query expression
+
+            var result = numbers.Where(x => x % 2 == 0).Select(x => x * 10);
+
+            // Execute the query
+
+            foreach(int x in result)
+            {
+                Console.WriteLine(x);
+            }
+
+            // Where: Filtrar a fonte de dados, gerar uma nova coleção contendo somente dados que atenderem predicados que informa como argumento da função
+
+            // (x => x % 2 == 0) : Predicado que vai dar true somente para numeros pares
+
+            // Select(x => x * 10): Pega cada elemento do resultado que contenha apenas numeros pares e multiplica por 10. Para aplicar uma função a cada 
+            // elemento da coleção original gerando uma nova função, com a exp lambda!
+        }
+    }
+}
---
+++ LINQ_Lambda/LINQ_Lambda/Entities/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace LINQ_Lambda.Entities
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public Category Category { get; set; } //Associação 
+
+        public override string ToString()
+        {
+            return Id + "," + Name + "," + Price.ToString("F2", CultureInfo.InvariantCulture) + "," + Category.Name + "," + Category.Tier;
+        }
+
+
+    }
+}
